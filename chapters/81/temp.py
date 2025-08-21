34ecc37eea6d641e5f45ad262ad9be98f7e014cb
freesemt <freesemt@gmail.com>
2025-08-21 17:38:15 +0900

    Add debug notebook

--- chapters/81/temp.py
+++ chapters/81/temp.py
@@ -9,11 +9,15 @@ assert get_version() >= '0.4.0', "This tutorial requires molass version 0.4.0 or
 
 import matplotlib.pylab as plt
 from molass.SEC.ColumnSimulation import get_animation
-anim = get_animation(num_frames=800, interval=100, close_plot=False, return_init=False)
+
+save_animation = False
+blit = save_animation
+
+anim = get_animation(num_frames=600, interval=100, seed=1234, close_plot=False, return_init=False, blit=blit, use_tqdm=False)
 
 plt.show()
 
-if False:
+if save_animation:
     to_mp4 = True
     if to_mp4:
         # Save the animation as an MP4 file
