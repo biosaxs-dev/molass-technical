f566b803b78b42a20ce2c0780b00aa1978ec39fa
freesemt <freesemt@gmail.com>
2025-08-22 17:05:04 +0900

    Add a temporary notebook for the 3D plot with view arrows

--- chapters/81/temp.py
+++ chapters/81/temp.py
@@ -10,7 +10,7 @@ assert get_version() >= '0.4.0', "This tutorial requires molass version 0.4.0 or
 import matplotlib.pylab as plt
 from molass.SEC.ColumnSimulation import get_animation
 
-save_animation = False
+save_animation = True
 blit = save_animation
 
 anim = get_animation(num_frames=600, interval=100, seed=1234, close_plot=False, return_init=False, blit=blit, use_tqdm=False)
@@ -21,7 +21,7 @@ if save_animation:
     to_mp4 = True
     if to_mp4:
         # Save the animation as an MP4 file
-        anim.save("sec-saxs.mp4", writer="ffmpeg", fps=30)
+        anim.save("sec-saxs.mp4", writer="ffmpeg", fps=20)
     else:
         # Save the animation as an HTML file
         anim.save("sec-saxs.gif")
